78dc212eff6b77259bd626f80ee0e069fee946b8
Mao-Lin Chiu <mao.chiu.astral@gmail.com>
2018-08-25 03:15:43 +0800

    Defined database models

--- blabber/models.py
+++ blabber/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import Truncator
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom user model"""
+    GENDER_CHOICES = {
+        ('M', 'Male'),
+        ('F', 'Female'),
+    }
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    friends = models.ManyToManyField('self', blank=True)
+
+
+class Post(models.Model):
+    text = models.TextField()
+    file_upload = models.FileField(upload_to='post_file', null=True, blank=True)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    date_posted = models.DateTimeField(auto_now_add=True)  # Date and time set at object creation
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+    def __str__(self):
+        truncated_text = Truncator(self.text)
+        return truncated_text.chars(30)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    commented_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    commented_to = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    date_commented = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+    def __str__(self):
+        truncated_text = Truncator(self.text)
+        return truncated_text.chars(30)
+
+
+class Message(models.Model):
+    body = models.CharField(max_length=1000)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages_sent')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages_received')
+    date_delivered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        truncated_body = Truncator(self.body)
+        return truncated_body.chars(30)
