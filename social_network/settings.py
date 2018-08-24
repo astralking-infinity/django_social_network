00bf3ed613118f5244329c76be82484ab2c7657e
Mao-Lin Chiu <mao.chiu.astral@gmail.com>
2018-08-25 03:14:11 +0800

    Updated

--- social_network/settings.py
+++ social_network/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'blabber.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR + '/media/')
