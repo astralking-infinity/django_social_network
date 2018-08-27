5b0fdf66945322a69bfd944ca9cd32d9318a5a5d
Mao-Lin Chiu <mao.chiu.astral@gmail.com>
2018-08-27 17:04:24 +0800

    Added form to edit profile photo

--- blabber/forms.py
+++ blabber/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Post
+from .models import Post, User
 
 
 class PostForm(forms.ModelForm):
@@ -8,3 +8,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['text', 'photo']
+
+
+class AvatarForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['avatar']
