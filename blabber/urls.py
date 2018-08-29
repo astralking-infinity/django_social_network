30dca8605bf1a50eea0cf646a2f9bcb92f11e6bd
Mao-Lin Chiu <mao.chiu.astral@gmail.com>
2018-08-29 19:34:49 +0800

    Defined url routing for like feature

--- blabber/urls.py
+++ blabber/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('<slug:username>/', views.ProfileView.as_view(), name='profile'),
     path('avatar/change/<int:pk>/', views.AvatarView.as_view(), name='avatar'),
+    path('posts/<int:pk>/like/', views.LikeToggleView.as_view(), name='like_toggle'),
 ]
