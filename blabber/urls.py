6c2146b2cc4cefbc8bb414828cedb4c7ff5c4fd6
Mao-Lin Chiu <mao.chiu.astral@gmail.com>
2018-09-07 23:08:01 +0800

    Defined url route for adding post and comment.

--- blabber/urls.py
+++ blabber/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<slug:username>/', views.ProfileView.as_view(), name='profile'),
     path('avatar/change/<int:pk>/', views.AvatarView.as_view(), name='avatar'),
     path('posts/<int:pk>/like/', views.LikeToggleView.as_view(), name='like_toggle'),
+    path('posts/add_post/', views.PostFormView.as_view(), name='add_post'),
+    path('posts/<int:pk>/comment/', views.CommentFormView.as_view(), name='comment'),
 ]
